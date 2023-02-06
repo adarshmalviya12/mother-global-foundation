ded559badce1cbae9bb1584b378a2ea5cd560678
Adarsh Malviya <adarshmalviya1199@gmail.com>
2023-02-06 13:13:22 +0530

    updates in footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.css'
+import { NavLink } from 'react-router-dom'
 function Footer() {
     return (
         <>
@@ -11,21 +12,20 @@ function Footer() {
                             <img src={require('../../assests/logo.png')} alt="loading..." />
                         </div>
                         <ul className='footer__links footer-content'>
-                            <li>Home</li>
-                            <li>Gallary</li>
-                            <li>Our Donars</li>
-                            <li>About us</li>
-                            <li>contact us</li>
+                            <li><NavLink to=''>Home</NavLink></li>
+                            <li><NavLink to='gallary'>Gallary</NavLink></li>
+                            <li><NavLink to="donar">Our Donars</NavLink></li>
+                            <li> <NavLink to="about">About us</NavLink> </li>
+                            <li><NavLink to='contact'>Contact us</NavLink > </li>
                         </ul>
                         <div className="footer__contacts footer-content">
                             <p className="footer__contacts-mail">
-                                sindhutaisapkalfoundation@gmail.com
+                                <i class="fa-solid fa-envelope" /> :- sindhutaisapkalfoundation@gmail.com
                             </p>
                             <p className="footer__contacts-no">
-                                <div className="no1">no - <span>9767929394 / 9422232044</span></div>
+                                <div className="no1"><i class="fa-solid fa-phone"></i> :- <a rel="stylesheet" href="tel:+919767929394">9767929394</a>  /  <a rel="stylesheet" href="tel:+919422232044" >9422232044</a></div>
                             </p>
-                            <p className="footer__contacts-address">
-                                Savitribai Fule mulinche vastigruh lal bunglow near Sipna Collage Upper Plato Chikhaldara, Dist. Amravati Maharashtra India Pin- 444807</p>
+                            <p className="footer__contacts-address"><i class="fa-solid fa-location-dot"> </i> :- Savitribai Fule mulinche vastigruh lal bunglow near Sipna Collage Upper Plato Chikhaldara, Dist. Amravati Maharashtra India Pin- 444807</p>
                         </div>
                     </div>
                 </div>
